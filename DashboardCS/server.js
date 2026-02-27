271b91c8735ffadc6ff2239576aa738efbc86107
arielmtnz <arielmtnz@gmail.com>
2026-02-27 17:38:15 -0300

    Ajustes en la definición de la cabecera y el footer

--- DashboardCS/server.js
+++ DashboardCS/server.js
@@ -1,17 +1,34 @@
+require('dotenv').config();   // ← Cargar variables .env
+
 const express = require('express');
 const oracledb = require('oracledb');
 const cors = require('cors');
+const path = require('path');
+
 const app = express();
+app.use(express.static("public"))
+
 app.use(cors());
-const PORT = 3000;
+
+// Puerto desde .env
+const PORT = process.env.PORT || 3000;
+
+// Ruta absoluta al archivo JSON
+const cabeceraPath = path.join(__dirname, 'config', 'cabecera.json');
+
+// Endpoint REST para servir el JSON
+app.get('/cabecera', (req, res) => {
+  res.sendFile(cabeceraPath);
+});
 
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
+// Conexión usando variables .env
 async function getConnection() {
   return await oracledb.getConnection({
-    user: "carlosm",
-    password: "cmartinez",
-    connectString: "199.1.1.36:1521/unicentro"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    connectString: process.env.DB_CONNECT
   });
 }
 
@@ -22,7 +39,7 @@ app.get('/clientes', async (req, res) => {
     conn = await getConnection();
 
     const result = await conn.execute(`
-     select * from adcc.pos_gift_card
+      select * from adcc.pos_gift_card
     `);
 
     res.json(result.rows);
@@ -40,4 +57,8 @@ app.get('/clientes', async (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "dashboard.html"));
 });
